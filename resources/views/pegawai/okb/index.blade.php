b0c9412c7adcb832f6fb4595f1ea0594fe768595
Your Name <you@example.com>
2025-01-10 17:08:43 +0700

    aura 4

--- resources/views/pegawai/okb/index.blade.php
+++ resources/views/pegawai/okb/index.blade.php
@@ -17,35 +17,35 @@
           <!-- /.box-header -->
           <div class="box-body table-responsive no-padding">
             <table class="table table-hover">
-              <tbody><tr>
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Nama</th>
-                <th>Alamat</th>
-                <th>Nopol</th>
-                <th>Roda</th>
-                <th>Nama Sesuai STNK</th>
-                <th>Masa Berlaku</th>
-                <th>No HP</th>
-                <th>Dokumentasi</th>
-                <th>Aksi</th>
+              <tbody><tr style="background-color:#FFD700">>
+                <th style="border: 1px solid black">No</th>
+                <th style="border: 1px solid black">Tanggal</th>
+                <th style="border: 1px solid black">Nama</th>
+                <th style="border: 1px solid black">Alamat</th>
+                <th style="border: 1px solid black">Nopol</th>
+                <th style="border: 1px solid black">Roda</th>
+                <th style="border: 1px solid black">Nama Sesuai STNK</th>
+                <th style="border: 1px solid black">Masa Berlaku</th>
+                <th style="border: 1px solid black">No HP</th>
+                <th style="border: 1px solid black">Dokumentasi</th>
+                <th style="border: 1px solid black">Aksi</th>
               </tr>
               @foreach ($data as $key => $item)
               <tr>
-                <td>{{1 + $key}}</td>
-                <td>{{$item->created_at}}</td>
-                <td>{{$item->nama}}</td>
-                <td>{{$item->alamat}}</td>
-                <td>{{$item->nopol}}</td>
-                <td>{{$item->roda}}</td>
-                <td>{{$item->namapemiliksesuaistnk}}</td>
-                <td>{{$item->masalakupajak}}</td>
-                <td>{{$item->nohp}}</td>
-                <td>
+                <td style="border: 1px solid black">{{1 + $key}}</td>
+                <td style="border: 1px solid black">{{$item->created_at}}</td>
+                <td style="border: 1px solid black">{{$item->nama}}</td>
+                <td style="border: 1px solid black">{{$item->alamat}}</td>
+                <td style="border: 1px solid black">{{$item->nopol}}</td>
+                <td style="border: 1px solid black">{{$item->roda}}</td>
+                <td style="border: 1px solid black">{{$item->namapemiliksesuaistnk}}</td>
+                <td style="border: 1px solid black">{{$item->masalakupajak}}</td>
+                <td style="border: 1px solid black">{{$item->nohp}}</td>
+                <td style="border: 1px solid black">
                   <a href="/storage/uploads/{{$item->dokumentasi}}" target="_blank">lihat</a>
                 </td>
-                <td>
-                  <a href="/pegawai/data/okb/print/{{$item->id}}" class="btn btn-flat btn-sm btn-primary" target="_blank"><i class="fa fa-print"></i> print</a>
+                <td style="border: 1px solid black">
+                  <a href="/pegawai/data/okb/print/{{$item->id}}" class="btn btn-flat btn-sm btn-warning" target="_blank"><i class="fa fa-print"></i> print</a>
                   <a href="/pegawai/data/okb/edit/{{$item->id}}" class="btn btn-flat btn-sm btn-primary"><i class="fa fa-edit"></i> Edit</a>
                   <a href="/pegawai/data/okb/delete/{{$item->id}}" class="btn btn-flat btn-sm btn-danger" onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i> Delete</a>
                 </td>
